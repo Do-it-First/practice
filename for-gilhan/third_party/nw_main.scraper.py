d89db5dc696611d8f2e464959127e694709641c5
gilhan <rlfgks97@gmail.com>
2022-03-19 16:32:40 +0900

    refactor: Change files' name

--- for-gilhan/third_party/nw_main.scraper.py
+++ for-gilhan/third_party/nw_main.scraper.py
@@ -19,48 +19,20 @@ with open('NW.html', 'w') as html_file:
     html_file.write(doc)
 
 col_inners = soup.find_all('div', class_='col_inner')
-
-
-print(type(col_inners))
-
-print("col_inners: ", type(col_inners))
 i = 0
+n = 0
+d = week[n]
 for col_inner in col_inners:
-		print("child:1 ", list(col_inner.children)[0])
-		print('!'*100)
 		print("child:2", list(col_inner.children)[1]) # @요 웹툰
-		print('!'*100)
-		print("child:3 ", list(col_inner.children)[2])
-		print('!'*100)
-		# print("child:4 ", list(col_inner.children)[3]) # ul
-		print("child: ", len(list(col_inner.children)[3])) # ul
-
+		day = list(col_inner.children)[1].text
 		for k in list(col_inner.children)[3]('a'):
+				i = i + 1
 				if k.get('title') != None:
-					print('o'*80)
-					print(type(k))
-					print("title: ", k.get('title'))
-					print("link: ", 'https://comic.naver.com' + k.get('href'))
+						title = k.get('title')
+						link = 'https://comic.naver.com' + k.get('href')
 				else:
-					print('!'*80)
-					print(type(k))
-					print("img: ", k.img['src'])
-					print(k)
-				print("^^"*30)
-
-
-		print('!'*100)
-		print("child:5 ", list(col_inner.children)[4])
-		print('!'*100)
+						thumbnail = k.img['src']
+		print(week[n], ":", i/2, "개")
+		i = 0
+		n = n + 1
 		
-		print("col_inner:", type(col_inner))
-
-		print("col_inner.h4: ", col_inner.h4.text) # @요 웹툰
-		i = i + 1
-		child = col_inner.descendants
-		print("type of child: ", type(child)) # generator
-		# print("child: ", list(child))
-		print(i)
-		print(input())
-		print('*'*100)
-
