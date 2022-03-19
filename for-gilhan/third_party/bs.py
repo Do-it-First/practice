1cfbb12f5107fc743cb24baf8fbd7a5c848c147e
gilhan <rlfgks97@gmail.com>
2022-03-19 11:45:51 +0900

    chore:

--- for-gilhan/third_party/bs.py
+++ for-gilhan/third_party/bs.py
@@ -16,6 +16,17 @@ print("html: ", type(html))
 print("soup: ", type(soup))
 
 
-# all_webtoons = soup.find_all('div', class_='list_area daily_all')
-# print(type(soup))
-# print(type(all_webtoons))
+all_webtoons = soup.find('div', class_='list_area daily_all')
+i = 0
+for tag in all_webtoons:
+		print("teg len: ", len(tag))
+		print("type: ", type(tag))
+		print(tag)
+		i = i + 1
+		print(i)
+		print("!"*200)
+
+print(type(all_webtoons[0]))
+
+content > div.list_area.daily_all > div:nth-child(1) > div > ul > li:nth-child(1) > div > a
+content > div.list_area.daily_all > div.col.col_selected > div > ul > li:nth-child(1) > div > a > span
