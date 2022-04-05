7182a39f85e2ccc5a95724f8377b936c2b0088b1
hanna <aaa5011@naver.com>
2022-04-05 16:10:35 +0900

    webtoon_monday_namuwiki

--- for-hanna/namuwiki.py
+++ for-hanna/namuwiki.py
@@ -1,31 +1,40 @@
 from selenium import webdriver
 from openpyxl import load_workbook, Workbook
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import StaleElementReferenceException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+from openpyxl.styles import Font
 from selenium import webdriver
 from openpyxl import load_workbook, Workbook
 
 def namuwiki_crawl(workbook, file_name, url, sheet):
-    chromedriver = "C:\manna\chromedriver.exe"
+    chromedriver = "C:\hanna\practice\for-hanna\chromedriver.exe"
     driver = webdriver.Chrome(chromedriver)
+    # url = "https://namu.wiki/w/%EB%84%A4%EC%9D%B4%EB%B2%84%20%EC%9B%B9%ED%88%B0/%EC%97%B0%EC%9E%AC%20%EB%AA%A9%EB%A1%9D"
     driver.get(url)
 
     #웹툰 정보
-    webtoon_genre_a = '//*[@id="wd8ZpUELU"]/article/div[4]/div/div/div/div/div/div[6]/div/div/div/div/div/div/div/div/div/div[6]/div[1]/div/div/table/tbody/tr[3]/td[2]/div/a'  
-    webtoon_genre_b = '//*[@id="wd8ZpUELU"]/article/div[4]/div/div/div/div/div/div[6]/div/div/div/div/div/div/div/div/div/div[6]/div[1]/div/div[3]/table/tbody/tr[3]/td[2]/div/a'
-    webtoon_genre_c = '//*[@id="wd8ZpUELU"]/article/div[4]/div/div/div/div/div/div[6]/div/div/div/div/div/div/div/div/div/div[6]/div[1]/div/div[2]/table/tbody/tr[3]/td[2]/div/a'
-    webtoon_check_genre = '//*[@id="wd8ZpUELU"]/article/div[4]/div/div/div/div/div/div[6]/div/div/div/div/div/div/div/div/div/div[6]/div[1]/div/div/table/tbody/tr[3]/td[1]/div/strong/span'
+    webtoon_genre = '//*[@id="wd8ZpUELU"]/article/div[4]/div/div/div/div/div/div[6]/div/div/div/div/div/div/div/div/div/div[6]/div[1]/div/div/table/tbody/tr[3]/td[2]/div/a'  
+        webtoon_check_genre = '//*[@id="wd8ZpUELU"]/article/div[4]/div/div/div/div/div/div[6]/div/div/div/div/div/div/div/div/div/div[6]/div[1]/div/div/table/tbody/tr[3]/td[1]/div/strong/span'
 
+# if webtoon_check_genre == "장르":
+#     print("테스트 완") # 테스트 통과
+
+    webtoon_genre = '//*[@id="wd8ZpUELU"]/article/div[4]/div/div/div/div/div/div[6]/div/div/div/div/div/div/div/div/div/div[6]/div[1]/div/div/table/tbody/tr[3]/td[2]/div'  
+    text = driver.find_elements_by_xpath(webtoon_genre) #ts물, 로맨스, ..
+
+    check_genre = ""
+    total_genre = ""
     try:
-        text = driver.find_elements_by_xpath(webtoon_genre_a) #ts물, 로맨스, ..
-        check_genre = ""
-        total_genre = ""
-        try:
-            check_genre = driver.find_element_by_xpath(webtoon_check_genre).text
-            if len(text) > 0:
-                if check_genre == "장르":
-                    genre_list = []
-                    cnt = 0
+        check_genre = driver.find_element_by_xpath(webtoon_check_genre).text
+        if len(text) > 0:
+            if check_genre == "장르":
+                genre_list = []
+                cnt = 0
+                try:
                     for zz in text:
                         if cnt == 0:
                             cnt += 1
@@ -33,33 +42,10 @@ def namuwiki_crawl(workbook, file_name, url, sheet):
                         else:
                             genre_list.append(zz.text)
                             total_genre = list(total_genre) + genre_list
-        except NoSuchElementException:
-            pass
-    except ValueError:
-        text_b = driver.find_elements_by_xpath(webtoon_genre_b)
-        total_genre = ""
-        genre_b_list = []
-        cnt = 0
-        for jj in text_b:
-            if cnt == 0:
-                cnt += 1
-                total_genre = jj.text_b   
-            else:
-                genre_b_list.append(jj.text_b)
-                total_genre = list(total_genre) + genre_b_list
-                if len(genre_b_list) == 0:
-                    text_c = driver.find_elements_by_xpath(webtoon_genre_c)
-                    total_genre = ""
-                    genre_c_list = []
-                    cnt = 0
-                    for kk in text_c:
-                        if cnt == 0:
-                            cnt += 1
-                            total_genre = kk.text_c
-                        else:
-                            genre_c_list.append(kk.text_c)
-                            total_genre = list(total_genre) + genre_c_list
-            
+                except ValueError:
+                    pass
+    except NoSuchElementException:
+        pass
     except StaleElementReferenceException:
         print("StaleElementReferenceException")
         return total_genre
@@ -70,11 +56,10 @@ def namuwiki_crawl(workbook, file_name, url, sheet):
     workbook.save(file_name)
     return total_genre
 
-
+#기존 네이버 웹툰 url 엑셀 파일 불러오기
 file_name = "webtoon_mon.xlsx"
 workbook = load_workbook(file_name)
 sheet = workbook['Sheet']
-#print(sheet.max_row)
 cc = 1
 for i in sheet['B']:
     url = i.value
